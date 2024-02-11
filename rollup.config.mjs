66713acdc7468b2b315928b519782e7f82d3d808
austinlovesworking@gmail.com <austinlovesworking@gmail.com>
2024-02-11 16:07:51 -0700

    Update rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,8 @@
 import terser from "@rollup/plugin-terser";
 import postcss from "rollup-plugin-postcss";
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
 
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  modules: false,
   input: "dist/index.js",
   output: [
     {
@@ -17,7 +14,14 @@ export default {
       format: "esm",
     },
   ],
-  external: ["react", "react-dom", "@tanstack/react-query"],
+  external: [
+    "react",
+    "react-dom",
+    "@tanstack/react-query",
+    "superjson",
+    "fast-equals",
+    "socket.io-client",
+  ],
   plugins: [
     postcss({
       config: {
